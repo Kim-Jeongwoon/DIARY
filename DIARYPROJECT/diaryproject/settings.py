a4bbfa988436f566b66f6cbf3a4bfa8b67ab127f
unknown <00jungwoon@naver.com>
2021-05-21 13:48:20 +0900

    DIARY

--- DIARYPROJECT/diaryproject/settings.py
+++ DIARYPROJECT/diaryproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'diaryapp.apps.DiaryappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'diaryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['DIARYPROJECT/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
